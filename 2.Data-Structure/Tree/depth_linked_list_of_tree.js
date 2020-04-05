dc2c5c8553d3cb565198914868478bb4a8dc2e9a
edwinKR <edwin.k.kim@gmail.com>
2020-04-05 15:06:08 -0500

    CCI(4.3) - Solution approach (DFS & BFS)

--- 2.Data-Structure/Tree/depth_linked_list_of_tree.js
+++ 2.Data-Structure/Tree/depth_linked_list_of_tree.js
@@ -1,4 +1,4 @@
-const { BinaryTree, theTree2 } = require("./balanced_binary_tree");
+const { theTree2 } = require("./balanced_binary_tree");
 
 class LinkedListNode {
   constructor(value) {
@@ -23,14 +23,15 @@ class LinkedList {
       while (currentNode.next) {
         currentNode = currentNode.next;
       }
-      // console.log("After loop: ", currentNode);
+
       currentNode.next = newNode;
     }
     return;
   }
 }
 
-function generateListFromBinaryTree(root) {
+// BFS Approach: My solution
+function BFS_generateListFromBinaryTree(root) {
   if (!root) return null;
 
   const queue = [root];
@@ -65,7 +66,58 @@ function generateListFromBinaryTree(root) {
   return hashMap;
 }
 
-const testResult2 = generateListFromBinaryTree(theTree2.root);
-console.log("testResult >>>>>>>>>>", testResult2);
+const testResult = BFS_generateListFromBinaryTree(theTree2.root);
+console.log("testResult >>>>>>>>>>", testResult);
 console.log("testing depth 3=================");
-console.log(testResult2.get(3).head.next.next);
+console.log(testResult.get(3).head.next.next);
+
+// BFS Approach: Cracking the Coding Interview Solution
+function CCI_BFS_generateListFromBinaryTree(root) {
+  if (!root) return null;
+
+  const queue = [root];
+  const lists = [];
+  root.depth = 0;
+
+  while (queue.length) {
+    const currentNode = queue.shift();
+
+    if (!lists[currentNode.depth]) {
+      lists[currentNode.depth] = new LinkedList();
+    }
+
+    lists[currentNode.depth].add(currentNode.value);
+
+    if (currentNode.left) {
+      currentNode.left.depth = currentNode.depth + 1;
+      queue.push(currentNode.left);
+    }
+
+    if (currentNode.right) {
+      currentNode.right.depth = currentNode.depth + 1;
+      queue.push(currentNode.right);
+    }
+  }
+
+  return lists;
+}
+
+// DFS Approach using memoization
+function DFS_generateListFromBinaryTree(root, lists = [], depthIdx = 0) {
+  if (!root) return; // Base Case
+
+  if (!lists[depthIdx]) {
+    lists[depthIdx] = new LinkedList();
+  }
+
+  lists[depthIdx].add(root.value);
+
+  DFS_generateListFromBinaryTree(root.left, lists, depthIdx + 1);
+  DFS_generateListFromBinaryTree(root.right, lists, depthIdx + 1);
+
+  return lists;
+}
+
+const testResult2 = DFS_generateListFromBinaryTree(theTree2.root);
+console.log("testResult2 >>>>>>>>>>", testResult2);
+console.log("testResult2 depth1>>>>>> ", testResult2[1]);
